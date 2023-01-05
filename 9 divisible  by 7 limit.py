2bf3678cb70229ee5579029a36f974204264017c
kmvishnu625 <40417939+kmvishnu625@users.noreply.github.com>
2023-01-05 14:37:05 +0530

    Update 9 divisible  by 7 limit.py

--- 9 divisible  by 7 limit.py
+++ 9 divisible  by 7 limit.py
@@ -1,7 +1,12 @@
 sum=0
 no=0
-for i in range(100,200):
+i=100
+while I!=NULL
     if i%7==0:
         sum+=i
         no+=1
+    else:
+        continue
+    if i==200:
+       break
 print("no. of integer divisible by 7 is {0} and sum is {1}".format(no,sum))
